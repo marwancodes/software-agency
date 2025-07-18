251debd3fb8d088d73e96c92df60a8e2fbbc6dc8
marwancoder <marwane.ouarradi@gmail.com>
2025-07-18 20:38:12 +0100

    Completed Navbar

--- components/Home/Nav/Navbar.tsx
+++ components/Home/Nav/Navbar.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import { GiEagleEmblem } from 'react-icons/gi';
 import { navLinks } from '@/constants/constant';
 import Link from 'next/link';
 import { HiBars3BottomRight } from 'react-icons/hi2';
 
 
-const Navbar = () => {
+type Props = {
+    openNav: ()=> void;
+}
+
+const Navbar = ({openNav}: Props) => {
+
+  const [navBg, setNavBg] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+        if (window.scrollY >= 90) { setNavBg(true)};
+        if (window.scrollY < 90) { setNavBg(true)};
+    };
+
+    window.addEventListener('scroll', handler);
+
+    return () => { window.removeEventListener('scroll', handler); }
+
+  }, []);
+
+
   return (
-    <div className='transition-all duration-200 h-[10vh] z-[100] fixed w-full bg-blue-900'>
+    <div className={`transition-all ${navBg ? "bg-blue-900 shadow-md" : "fixed"} duration-200 h-[10vh] z-[100] fixed w-full bg-[#121212]`}>
         <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
             {/* LOGO */}
             <div className='flex items-center space-x-2'>
@@ -41,10 +63,10 @@ const Navbar = () => {
             </div>
             {/* ThemeToggler */}
             {/* Burger Menu */}
-            <HiBars3BottomRight className='w-8 h-8 cursor-pointer text-white lg:hidden' />
+            <HiBars3BottomRight onClick={openNav} className='w-8 h-8 cursor-pointer text-white lg:hidden' />
         </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
--- components/Home/Nav/ResponsiveNav.tsx
+++ components/Home/Nav/ResponsiveNav.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import Navbar from './Navbar'
-// import MobileNav from './MobileNav'
+'use client';
 
-const ResponsiveNav = () => {
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import MobileNav from './MobileNav';
+
+
+type Props = {
+    openNav: ()=> void;
+}
+
+const ResponsiveNav = ({}: MobileNavProps) => {
+
+    const [showNav, setShowNav] = useState(false);
+
+    const handleShowNav = () => {
+        setShowNav(true);
+      }
+    
+      const handleCloseNav = () => {
+        setShowNav(false);
+      }
+
   return (
     <div>
-        <Navbar />
-        {/* <MobileNav /> */}
+        <Navbar openNav={handleShowNav} />
+        <MobileNav showNav={showNav} closeNav={handleCloseNav}/>
     </div>
   )
 }
